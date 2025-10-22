04c13ae34d7248afaa80c0346bed1fd4cd75ce44
Anas Zekra <anaszekra19@gmail.com>
2025-10-22 22:15:39 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKereTekrar {
+    public static void main(String[] args) {
+            System.out.println("javaya hos geldiniz");
+            System.out.println("javaya hos geldiniz");
+            System.out.println("javaya hos geldiniz");
+            System.out.println("javaya hos geldiniz");
+            System.out.println("javaya hos geldiniz");
+        }
+    }
