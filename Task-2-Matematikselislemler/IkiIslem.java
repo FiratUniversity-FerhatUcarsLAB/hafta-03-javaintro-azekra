ad6cde9bfb36363f922a29056350fd72079033ce
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 11:46:07 +0300

    Update IkiIslem.java

--- Task-2-Matematikselislemler/IkiIslem.java
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -1,6 +1,11 @@
-public class Ikilslem {
+public class IkiIslem {
     public static void main(String[] args) {
-        System.out.println("Toplam 1+2+3+4+5+6+7+8+9 = : " + (1+2+3+4+5+6+7+8+9));
-    //tırnak içindekiler oldukları gibi compiler okuyacak sadece parantez içinde olanlara matematiksel işlem uygulanacak
+        // Pay ve payda
+        double pay = (9.5 * 4.5) - (2.5 * 3);
+        double payda = 45.5 - 3.5;
+        // Sonuç hesaplama 
+        double sonuc = pay / payda;
+        // Sonucu 4 ondalık basamakla yazdırma
+        System.out.printf("Sonuc: %.4f\n", sonuc);
     }
 }
