04e7ab919dde6808e78671fe9667dbd2f0f97a39
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 12:09:15 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,8 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+        int x = 5;
+        int sonuc = x * (x - 1) * (x - 2) * (x - 3) * (x - 4);
+        System.out.println("5! = " + sonuc);
+    }
+    
+}
