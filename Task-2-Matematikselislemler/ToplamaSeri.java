19f1cdf85292618acba390c6015d44f7d6c9879c
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 11:10:16 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,6 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+        System.out.println("Toplam 1+2+3+4+5+6+7+8+9 = : " + (1+2+3+4+5+6+7+8+9));
+    //tırnak içindekiler oldukları gibi compiler okuyacak sadece parantez içinde olanlara matematiksel işlem uygulanacak
+    }
+}
