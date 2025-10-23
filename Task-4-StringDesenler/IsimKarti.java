183ea8a9506b84601d5c66defeb30ed3df07217f
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 13:22:18 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+    public static void main(String[] args)
+
+        String ad = "ENES";
+        String soyad = "ZUKRA";
+        String ogrNo = "250541616";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println("========================");
+        System.out.println("     OGRENCI KARTI     ");
+        System.out.println("========================");
+        System.out.println("Ad         : " + ad);
+        System.out.println("Soyad      : " + soyad);
+        System.out.println("Ogrenci No : " + ogrNo);
+        System.out.println("Bolum      : " + bolum);
+        System.out.println("========================");
+    }
+}
