b4e6b4f6c2bbe386b8836ab5a46fabfa94a7b771
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 18:22:57 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,10 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+
+        System.out.println("EEEEE     N   N    EEEEE    SSS  ");
+        System.out.println("E         NN  N    E       S     ");
+        System.out.println("EEE       N N N    EEE      SS   ");
+        System.out.println("E         N  NN    E          S  ");
+        System.out.println("EEEEE     N   N    EEEEE   SSS   ");
+    }
+}
