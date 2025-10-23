7d7779a0f6c0d7eb3197728b9083618874da9137
Anas Zekra <anaszekra19@gmail.com>
2025-10-23 13:26:32 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,16 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+
+        
+        System.out.println("Urun        Miktar    Fiyat");
+        System.out.println("-----------------------------");
+
+        
+        System.out.println("Elma          3        2.50");
+        System.out.println("Ekmek         1        5.00");
+        System.out.println("Sut           2        8.75");
+
+        System.out.println("-----------------------------");
+        System.out.println("Toplam                 24.00");
+    }
+}
